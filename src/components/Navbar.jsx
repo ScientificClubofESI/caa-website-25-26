2fb97901c134e3f6d59737dfc0f5ff2334e49fd8
yasser lakrid <yasserlakrid36@gmail.com>
2026-03-11 15:22:21 +0100

    add the navBAR

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,69 @@
+"use client"
+import "./NavBar.css"
+import { useEffect, useState } from "react";
 export default function Navbar() {
+  const [dropMenu , setDropMenu] = useState(false)
+  const [mobileView , setMobileView] = useState(false)
+  const [toggle , setToggel] = useState(false)
+
+  //track mobile view
+  useEffect(()=>{
+    const media = window.matchMedia("(max-width:750px)");
+    const handleChange = (e)=>{
+      setMobileView(e.matches)
+    }
+    setMobileView(media.matches);
+    media.addEventListener("change" , handleChange);
+    return () => media.removeEventListener("change", handleChange);
+  },[])
+
   return (
     <section>
-      <h1 className="text-4xl font-bold">
-        Hello, this is the Navbar section
-      </h1>
+      <div className="NavBar"> 
+         <div className="caaLogo">
+              <img src="/logo.png" alt="CAA Logo" />
+          </div>  
+     
+        { mobileView ? 
+
+        (
+            <div className="hamMenu" onClick={()=>setToggel((prev)=>!prev)}>
+              <img src="/menu.png" alt="CAA Logo" />
+            </div>
+
+          ) :(
+            <>
+            <div className="nav-links">
+                  <a href="#about">About</a>  
+                  <a href="#partners">Partners</a>
+                  <a href="#workshops">Workshops</a>
+                  <a href="#willayas">Willayas</a>
+                  <a href="#FAQ">FAQs</a>
+                  <a href="#contact">Contact</a>
+                
+              </div>
+          <div className="register">
+                <button className="registerBtn">Register</button>
+          </div>
+            </>
+          
+          )
+
+        }
+        
+
+      
+      </div>
+      {toggle && mobileView &&
+       <div className="nav-links-toggled" onClick={()=>setToggel(false)}>              
+                  <a href="#about" onClick={()=>console.log("about")}>About</a> 
+                  <a href="#partners">Partners</a>
+                  <a href="#workshops">Workshops</a>
+                  <a href="#willayas">Willayas</a>
+                  <a href="#FAQ">FAQs</a>
+                  <a href="#contact">Contact</a>
+              </div>
+        } 
     </section>
   );
 }

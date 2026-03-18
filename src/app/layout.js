1174f9f2cc18fcb6fee9d1e6c91d38bea76089e3
chouider ikram <124279334+ikramchouider@users.noreply.github.com>
2026-03-18 21:10:10 +0100

    .

--- src/app/layout.js
+++ src/app/layout.js
@@ -8,13 +8,25 @@ const quicksand = Quicksand({
 });
 
 export const metadata = {
-  title: "CAA 4",
-  description: "CAA 4 — CSE AROUND ALGERIA 4.0",
+  title: "CAA 4.0",
+  description: "CSE Around Algeria unites learners and innovators, boosting collaboration and tech community growth.",
   icons: {
     icon: "/logo.png",
     shortcut: "/logo.png",
     apple: "/logo.png",
-  }
+  },
+    openGraph: {
+    title: 'CAA 4.0',
+    description: 'CSE Around Algeria unites learners and innovators, boosting collaboration and tech community growth.',
+    images: ['/images/cover.png'],
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'DataHack 3.0',
+    description: 'CSE Around Algeria unites learners and innovators, boosting collaboration and tech community growth.',
+    images: ['/images/cover.png'],
+  },
 };
 
 export default function RootLayout({ children }) {
